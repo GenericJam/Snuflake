cad9c767bcd299f7c98795daae07e5faf8c736fc
genericjam <genericjam@gmail.com>
2021-04-25 19:39:51 +0200

    Fix Application.start

--- lib/snuflake/application.ex
+++ lib/snuflake/application.ex
@@ -6,14 +6,20 @@ defmodule Snuflake.Application do
 
   require Logger
 
-  def start(node_id \\ 0) when node_id in 0..1023 do
-    GlobalId.start_link(node_id)
+  # Set this to change the node_id
+  @node_id 0
+
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
+  def start(_type, _args) do
+    GlobalId.start_link(@node_id)
   end
 
+  @spec get_id :: non_neg_integer
   def get_id do
     GlobalId.get_id()
   end
 
+  @spec get_id(non_neg_integer) :: non_neg_integer
   def get_id(last_id) do
     GlobalId.get_id(last_id)
   end
